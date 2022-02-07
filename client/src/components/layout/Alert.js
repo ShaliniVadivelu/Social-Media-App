8463ddba11787df5f6020e04c59303c8a1ed94fa
V.Shalini Srirama <shalinisrirama.v@gmail.com>
2022-02-07 14:30:19 +0530

    aligned

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -4,13 +4,16 @@ import {connect } from 'react-redux';
 // alerts is a destructor here
 const Alert = ({ alerts}) => 
 //condition if the below cdn satisfied it will print corresponding alert type.here we have only one which is for pw.
-alerts !== null && alerts.length >0 && alerts.map(alert => {
-    <div key={alert.id} className ={'alert alert-${alert.alertType}'}
+alerts !== null && 
+alerts.length >0 && 
+alerts.map(alert => {
+    <div key={alert.id} className ={'alert alert-${alert.alertType}'}>
+    {alert.msg}
+    </div>
 });
 
 Alert.propTypes = {
     alerts: PropTypes.array.isRequired
-
 }
 
 const mapStateToProps =  state => ({
