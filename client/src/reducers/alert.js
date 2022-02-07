594d7b703328c20745943ca36bb5f9abe00b2113
V.Shalini Srirama <shalinisrirama.v@gmail.com>
2022-02-07 11:08:30 +0530

    insert and remove alert

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,5 +1,16 @@
-const initialState =[]; 
+import { SET_ALERT, REMOVE_ALERT} from '../actions/types';
 
-export default function(state= initialState, action) {
-    Switch(action.type)
+const initialState =[];
+
+export default function (state = initialState, action) {
+    const { type, payload } = action;
+
+    switch (type) {
+        case SET_ALERT:
+        return [...state, payload];
+        case REMOVE_ALERT:
+            return state.filter(alert =>alert.id1= payload);
+        default:
+            return state;
+    }
 }
