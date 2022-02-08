a795d78240e692c06f7150d4d7f885a0f2174506
V.Shalini Srirama <shalinisrirama.v@gmail.com>
2022-02-08 17:21:18 +0530

    added register sucess nd fail

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,5 @@
 export const SET_ALERT = 'SET_ALERT';  //used to see all the actions in one place
 export const REMOVE_ALERT = 'REMOVE_ALERT';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+
