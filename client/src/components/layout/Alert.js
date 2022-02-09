53a880ab8b78d30b90457215c7ca77c1a9fe6925
V.Shalini Srirama <shalinisrirama.v@gmail.com>
2022-02-09 12:37:16 +0530

    corrected and added { ->( and ';'

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 import {connect } from 'react-redux';
 // alerts is a destructor here
 const Alert = ({ alerts}) => 
-//condition if the below cdn satisfied it will print corresponding alert type.here we have only one which is for pw.
+// if the below cdn satisfied it will print corresponding alert type.here we have only one which is for pw.
     alerts !== null && 
-    alerts.length >0 &&
-    alerts.map(alert => {
+    alerts.length > 0 &&
+    alerts.map(alert => (
     <div key={alert.id} className ={`alert alert-${alert.alertType}`}>
     {alert.msg}
     </div>
-});
+    ));
 
 Alert.propTypes = {
     alerts: PropTypes.array.isRequired
-}
+};
 
 const mapStateToProps =  state => ({
     alerts: state.alert
-})
+});
 
 export default connect(mapStateToProps) (Alert);
