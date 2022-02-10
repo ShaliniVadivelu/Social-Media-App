f1a1817b7333eaa06984b010e05a8d4aa2755675
V.Shalini Srirama <shalinisrirama.v@gmail.com>
2022-02-10 16:26:33 +0530

    enabled dashboard

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+// Here we are going to fetch all of the data using  in action and bring it in from redux state and pass it down to other components like exp and edu
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Dashboard = props => {
+    return <div> Dashboard </div>;
+};
+
+Dashboard.propTypes = {};
+
+export default Dashboard;
