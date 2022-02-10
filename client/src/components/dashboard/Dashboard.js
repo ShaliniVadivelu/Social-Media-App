8fe8174202c7e8b7edc8ce20eb07029b36656f53
V.Shalini Srirama <shalinisrirama.v@gmail.com>
2022-02-10 17:35:23 +0530

    imported get profile and added to dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,11 +1,26 @@
 // Here we are going to fetch all of the data using  in action and bring it in from redux state and pass it down to other components like exp and edu
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
 
-const Dashboard = props => {
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+    useEffect (() => {
+        getCurrentProfile();
+    }, []);
+
     return <div> Dashboard </div>;
 };
 
-Dashboard.propTypes = {};
+Dashboard.propTypes = {
+    getCurrentProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    profile: PropTypes.object.isRequired
+};
 
-export default Dashboard;
+const mapStateToProps = state => ({
+    auth: state.auth,
+    profile: state.profile
+});
+
+export default connect (mapStateToProps, { getCurrentProfile}) (Dashboard);
