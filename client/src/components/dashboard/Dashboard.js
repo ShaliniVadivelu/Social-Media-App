3bbf7503482547a9b63bc257dc48836d836bb435
V.Shalini Srirama <shalinisrirama.v@gmail.com>
2022-02-13 21:04:36 +0530

    imported dashboard action and added spinner for loading

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,25 +5,33 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 // Adding the spinner to the dashboard
 import Spinner from '../layout/Spinner'; 
+import DashboardActions from './DashboardActions';
 import { getCurrentProfile } from '../../actions/profile';
  
 const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
     useEffect (() => {
         getCurrentProfile();
     }, []);
+
     // Displaying the user name, If user has no profile it will ask us to clik the profile creation button to create profile
-    return loading && profile === null ? <Spinner /> : <Fragment>
+    return loading && profile === null ? (<Spinner />): (<Fragment>
         <h1 className ="large text-primary"> Dashbaord</h1>
         <p className ="lead">
             <i className ="fas fa-user"></i> Welcome {user && user.name} 
         </p>
-        {profile !== null ? 
-            <Fragment>has</Fragment> : 
-            <Fragment>
+        { profile !== null ? (
+            <Fragment> 
+            <DashboardActions /> 
+            </Fragment> 
+            ) : ( 
+                <Fragment>
             <p> You have not setup a profile yet, please add some info </p>
             <Link to ='/create-profile' className="btn btn-primary my-1"> Create Profile </Link>
-            </Fragment>}
-        </Fragment>;
+            </Fragment>
+            )
+        }
+        </Fragment>
+        );
 };
 
 Dashboard.propTypes = {
