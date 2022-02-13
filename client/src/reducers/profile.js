d63d4970211f044446d4bd2372b2fc35eb7d0097
V.Shalini Srirama <shalinisrirama.v@gmail.com>
2022-02-13 21:04:55 +0530

    imported updated profile

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,5 +1,5 @@
 /*it will get all our profile and also others profile if we visit their page , if there is no profile for the user it will give profile error status and if we want to move to new user previous users profile need to cleard */
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from "../actions/types";
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from "../actions/types";
 
 const initialState = {
     profile: null,
@@ -15,11 +15,13 @@ export default function(state= initialState,action) {
 
     switch(type) {
         case GET_PROFILE:
+        case UPDATE_PROFILE:
             return {
                 ...state,
                 profile: payload,
                 loading: false
             };
+        
         case PROFILE_ERROR:
             return {
                 ...state,
