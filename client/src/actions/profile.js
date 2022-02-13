c4d08f200e1b80c2ecffbb24bd2c414d46a31edc
V.Shalini Srirama <shalinisrirama.v@gmail.com>
2022-02-13 21:07:13 +0530

    imported update profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,7 +3,8 @@ import { setAlert } from './alert';
 
 import {
     GET_PROFILE,
-    PROFILE_ERROR
+    PROFILE_ERROR,
+    UPDATE_PROFILE
 } from './types';
 
 //Get current users profile
@@ -46,6 +47,72 @@ export const createProfile =(formData, history, edit= false) => async dispatch =
             history.push ('/dashboard');
         }
 
+    } catch (err) {
+        //if user forget anything tat will show in alert
+        const errors = err.response.data.errors;
+
+        if (errors) {
+            errors.forEach(error => dispatch (setAlert(error.msg, 'danger')));
+        }
+
+        dispatch ({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status}
+        });
+    }
+};
+
+// Add experience
+export const addExperience = (formData, history) => async dispatch => {
+     try  {
+        const config = {
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        }
+
+        const res = await axios.put('/api/profile/experience', formData, config);
+        dispatch ({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+        // setAlert to notify us whether profile updated or created
+        dispatch (setAlert('Experience Added', 'success'));
+        history.push ('/dashboard');
+        
+    } catch (err) {
+        //if user forget anything tat will show in alert
+        const errors = err.response.data.errors;
+
+        if (errors) {
+            errors.forEach(error => dispatch (setAlert(error.msg, 'danger')));
+        }
+
+        dispatch ({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status}
+        });
+    }
+}
+
+// Add Education
+export const addEducation = (formData, history) => async dispatch => {
+     try  {
+        const config = {
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        }
+
+        const res = await axios.put('/api/profile/education', formData, config);
+        dispatch ({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+        // setAlert to notify us whether profile updated or created
+        dispatch (setAlert('Education Added', 'success'));
+        history.push ('/dashboard');
+        
     } catch (err) {
         //if user forget anything tat will show in alert
         const errors = err.response.data.errors;
