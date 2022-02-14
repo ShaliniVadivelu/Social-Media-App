1d0fbd80044bb9faa2f83c3947247820fa97f45b
V.Shalini Srirama <shalinisrirama.v@gmail.com>
2022-02-14 14:28:19 +0530

    edited job to school

--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -81,7 +81,7 @@ const AddEducation = ({ addEducation, history}) => {
               toggleDisabled(!toDateDisabled);
               } } 
               /> {' '}
-              Current Job</p>
+              Current School </p>
         </div>
         <div class="form-group">
           <h4>To Date</h4>
