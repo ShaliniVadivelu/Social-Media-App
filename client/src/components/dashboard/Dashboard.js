d6621c0ee3743cb425df7798b18de7f619637183
V.Shalini Srirama <shalinisrirama.v@gmail.com>
2022-02-14 12:32:37 +0530

    import Experience and Education to dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,6 +6,8 @@ import {connect} from 'react-redux';
 // Adding the spinner to the dashboard
 import Spinner from '../layout/Spinner'; 
 import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
 import { getCurrentProfile } from '../../actions/profile';
  
 const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
@@ -22,6 +24,8 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
         { profile !== null ? (
             <Fragment> 
             <DashboardActions /> 
+            <Experience experience={profile.experience}/>
+            <Education education={profile.education}/>
             </Fragment> 
             ) : ( 
                 <Fragment>
