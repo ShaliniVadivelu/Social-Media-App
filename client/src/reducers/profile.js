e64e5f4e54f57487a30a4601c622479ed9c2e535
V.Shalini Srirama <shalinisrirama.v@gmail.com>
2022-02-15 18:10:12 +0530

    get profiles and repos

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,5 +1,5 @@
 /*it will get all our profile and also others profile if we visit their page , if there is no profile for the user it will give profile error status and if we want to move to new user previous users profile need to cleard */
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from "../actions/types";
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE, GET_PROFILES, GET_REPOS } from "../actions/types";
 
 const initialState = {
     profile: null,
@@ -21,7 +21,12 @@ export default function(state= initialState,action) {
                 profile: payload,
                 loading: false
             };
-        
+        case GET_PROFILES:
+            return {
+                ...state,
+                profile: payload,
+                loading: false
+            };
         case PROFILE_ERROR:
             return {
                 ...state,
@@ -33,6 +38,12 @@ export default function(state= initialState,action) {
                 ...state,
                 profile : null,
                 repos: [],
+                loading: false
+            };
+        case GET_REPOS:
+            return {
+                ...state,
+                repos: payload,
                 loading: false
             };
         default:
