1553289c627824f9f792ad51ee3f4902f37b51da
V.Shalini Srirama <shalinisrirama.v@gmail.com>
2022-02-19 16:19:36 +0530

    Added Comments

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,12 @@
+// React is a Opensource JS library for building user interface
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-//import reportWebVitals from './reportWebVitals';
 
+// DOM-Document Object Modal, is a program interface for web documents
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+// To render the React element into root DOM node, we are passing both(element and root) to ReactDOM.render()
 ReactDOM.render(
  <App />,
   document.getElementById('root')
