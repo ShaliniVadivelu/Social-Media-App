353b2ef0639f2b1532e8e7ee8ab357bacb0a7fe2
V.Shalini Srirama <shalinisrirama.v@gmail.com>
2022-02-20 22:19:03 +0530

    corrected spellings

--- client/src/components/profile/ProfileExperience.js
+++ client/src/components/profile/ProfileExperience.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
 
-const ProfileExperience = ({ experiene: {
+const ProfileExperience = ({ experience: {
     company,
     title,
     loaciton,
@@ -14,18 +14,18 @@ const ProfileExperience = ({ experiene: {
 }) => <div>
     <h3 className ="text-dark"> {company}</h3>
     <p> 
-        <Moment formt='YYYY/MM/DD'> {from} </Moment>-{!to ? 'Now' : <Moment format='YYYY/MM/DD'>{to} </Moment>}
+        <Moment format='YYYY/MM/DD'> {from} </Moment>-{!to ? 'Now' : <Moment format='YYYY/MM/DD'>{to} </Moment>}
     </p>
     <p>
-        <strong> Position: </strong>
+        <strong> Position: </strong> {title}
     </p>
     <p>
         <strong> Description </strong> {description}
     </p>
-    </div>
+    </div>;
 
 ProfileExperience.propTypes = {
-    experiene:PropTypes.array.isRequired
-}
+    experience:PropTypes.array.isRequired
+};
 
-export default ProfileExperience   
+export default ProfileExperience;
