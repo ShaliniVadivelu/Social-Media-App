2eef3840ae30d5af2789d3a26bb559b249d405c6
V.Shalini Srirama <shalinisrirama.v@gmail.com>
2022-02-21 23:04:46 +0530

    corrected postId to id

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -39,7 +39,7 @@ export const addLike = id => async dispatch => {
 };
 
 //Remove Like
-export const removeLike = postId => async dispatch => {
+export const removeLike = id => async dispatch => {
     try {
         const res = await axios.put(`/api/posts/unlike/${postId}`);
 
