6587bc46e879388c725e40b13af4c75e91846b39
V.Shalini Srirama <shalinisrirama.v@gmail.com>
2022-02-22 21:56:40 +0530

    Post component

---
+++ client/src/components/post.js/Post.js
@@ -0,0 +1,15 @@
+import React , {Fragment, useEffect} from 'react'
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import {getPost} from '../../actions/post';
+
+const Post = props => {
+  return (
+    <div>Post</div>
+  )
+}
+
+Post.propTypes = {}
+
+export default Post
