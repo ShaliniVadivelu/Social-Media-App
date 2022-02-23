0b74adfc5aa47c5924f5941ae3cddf0db606b365
V.Shalini Srirama <shalinisrirama.v@gmail.com>
2022-02-23 15:57:58 +0530

    corrected the spelling

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -21,5 +21,5 @@ export const UPDATE_LIKES = 'UPDATE_LIKES';
 export const DELETE_POST = 'DELETE_POST';
 export const ADD_POST = 'ADD_POST';
 export const GET_POST ='GET_POST';
-export const ADD_REMOVE ='ADD_REMOVE';
-export const REMOVE_POST ='REMOVE_POST';
+export const ADD_COMMENT ='ADD_COMMENT';
+export const REMOVE_COMMENT ='REMOVE_COMMENT';
